64613a76183a8aa464f47c3665bcdcf4e6dd8a6a
Adam Borowski <aborowski@atlassian.com>
2016-01-20 15:52:27 +0100

    initial_commit

---
+++ zad2/test1/BioInf2/js/features/home/main.js
@@ -0,0 +1,37 @@
+/**
+ * ******************************************************************************************************
+ *
+ *   Defines a home feature
+ *
+ *  @author  aborowski
+ *  @date    Jan 20, 2016
+ *
+ * ******************************************************************************************************
+ */
+'use strict';
+import FeatureBase from 'lib/FeatureBase';
+import Routes from './Routes';
+import HomeController from './controller/HomeController';
+import HomeService from './service/HomeService';
+import customTpl from './partials/custom.html';
+
+class Feature extends FeatureBase {
+
+    constructor() {
+        super('home');
+        this.routes = Routes;
+    }
+
+    execute() {
+        this.controller('HomeController', HomeController);
+        this.service('HomeService', HomeService);
+        this.run([
+            '$templateCache',
+            function($templateCache) {
+                $templateCache.put('customTpl', customTpl);
+            }
+        ]);
+    }
+}
+
+export default Feature;
